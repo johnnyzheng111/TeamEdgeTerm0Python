a0847f677b5b4f68fbbc46366b282bd4c9f1975d
johnny.zheng@csedge.org <johnny.zheng@csedge.org>
2022-07-08 15:00:55 -0700

    finished conditional game and functions challenges today

--- conditional_game.py
+++ conditional_game.py
@@ -1,4 +1,8 @@
-import random
+# Horoscope fortune teller game
+# import random
+strength = ""
+weaknesses = ""
+
 
 # -------------------------------------------- 
 
@@ -30,9 +34,206 @@ import random
 	
 
   # --------------------------------------------
+# we are making a horoscope fortune teller thing
+
+#Aquarius
+AquariusStr="Progressive, original, independent, humanitarian"
+AquariusWe="Runs from emotional expression, temperamental, #uncompromising, aloof"
 
 
+#Pisces
+PiscesStr="Compassionate, artistic, intuitive, gentle, wise, musical"
+PiscesWe="Fearful, overly trusting, sad, desire to escape reality, can be a victim or a martyr"
 
+
+#ARIES
+AriesStr="Courageous, determined, confident, enthusiastic, optimistic, honest, passionate"
+AriesWe="Impatient, moody, short-tempered, impulsive, aggressive"
+
+
+#Taurus
+TaurusStr="Reliable, patient, practical, devoted, responsible, stable"
+TaurusWe="Stubborn, possessive, uncompromising"
+
+
+#Gemini Traits
+GeminiStr ="Gentle, affectionate, curious, adaptable, ability to learn quickly and exchange ideas"
+GeminiWe ="Nervous, inconsistent, indecisive"
+
+
+#Cancer Traits
+CancerStr ="Tenacious, highly imaginative, loyal, emotional, sympathetic, persuasive"
+CancerWe ="Moody, pessimistic, suspicious, manipulative, insecure"
+
+
+# Leo Traits
+LeoStr ="Creative, passionate, generous, warm-hearted, cheerful, humorous"
+LeoWe= "Arrogant, stubborn, self-centered, lazy, inflexible"
+
+
+# Virgo Traits
+VirgoStr= "Loyal, analytical, kind, hardworking, practical"
+VirgoWe="Shyness, worry, overly critical of self and others, all work and no play"
+
+
+# Libra Traits
+LibraStr= "Cooperative,diplomatic, gracious, fair-minded, social"
+LibraWe= "Indecisive, avoids confrontations, will carry a grudge, self-pity"
+
+
+# Scorpio Traits
+ScorpioStr = "Strengths: Resourceful, powerful, brave, passionate, a true friend"
+ScorpioWe = "Distrusting, jealous, manipulative, violent"
+
+
+# Sagittarius Traits
+SagittariusStr = "Generous, idealistic, great sense of humor"
+SagittariusWe = "Promises more than can deliver, very impatient, will say anything no matter how undiplomatic"
+
+
+# Capricorn Traits
+CapricornStr = "Responsible, disciplined, self-control, good managers"
+
+CapricornWe = "Know-it-all, unforgiving, condescending, expecting the worst"
+
+
+print("Do you want to find out your horoscope?\nInput your birthday month:")
+
+user_month = input()
+user_month = str(user_month)
+print("Now input the day of the month you were born in:")
+
+user_day = input()
+user_day = int(user_day)
+# codnitionals for the horoscope signs
+# Aries (March 21 – April 19)
+# Taurus (April 20 – May 20)
+# Gemini (May 21 – June 20)
+# Cancer (June 21 – July 22)
+# Leo (July 23 – August 22)
+# Virgo (August 23 – September 22)
+# Libra (September 23 – October 22)
+# Scorpio (October 23 – November 21)
+# Sagittarius (November 22 – December 21)
+# Capricorn (December 22 – January 19)
+# Aquarius (January 20 – February 18)
+# Pisces (February 19 – March 20)
+astrology_sign = ""
+# determining the astrology sign
+
+# January to June
+
+if user_month == "January" and user_day <= 19:
+  astrology_sign = "Capricorn"
+  strengths = CapricornStr
+  weaknesses = CapricornWe
+elif user_month == "January" and user_day >= 20:
+  astrology_sign = "Aquarius"
+  strengths = AquariusStr
+  weaknesses = AquariusWe
+elif user_month == "January" and user_day >= 20:
+  astrology_sign = "Aquarius"
+  strengths = AquariusStr
+  weaknesses = AquariusWe
+elif user_month == "February" and user_day >= 19:
+  astrology_sign = "Pisces"
+  strengths = PiscesStr
+  weaknesses = PiscesWe
+elif user_month == "March" and user_day <= 20:
+  astrology_sign = "Pisces"
+  strengths = PiscesStr
+  weaknesses = PiscesWe
+elif user_month == "March" and user_day >= 21:
+  astrology_sign = "Aries" 
+  strengths = AriesStr
+  weaknesses = AriesWe
+elif user_month == "April" and user_day <= 19:
+  astrology_sign = "Aries" 
+  strengths = AriesStr
+  weaknesses = AriesWe
+elif user_month == "April" and user_day >= 20:
+  astrology_sign = "Taurus"
+  strengths = TaurusStr
+  weaknesses = TaurusWe
+elif user_month == "May" and user_day <= 20:
+  astrology_sign = "Taurus"
+  strengths = TaurusStr
+  weaknesses = TaurusWe  
+elif user_month == "May" and user_day >= 19:
+  astrology_sign = "Gemini"
+  strengths = GeminiStr
+  weaknesses = GeminiWe
+elif user_month == "June" and user_day <= 20:
+  astrology_sign = "Gemini"
+  strengths = GeminiStr
+  weaknesses = GeminiWe
+elif user_month == "June" and user_day >= 20:
+  astrology_sign = "Cancer"
+  strengths=CancerStr
+  weaknesses=CancerWe
+elif user_month == "July" and user_day <= 22:
+  astrology_sign = "Cancer"
+  strengths = CancerStr
+  weaknesses = CancerWe
+elif user_month == "July" and user_day >= 23: 
+  astrology_sign = "Leo"
+  strengths = LeoStr
+  weaknesses = LeoWe
+elif user_month == "August" and user_day <= 22: 
+  astrology_sign = "Leo"
+  strengths = LeoStr
+  weaknesses = LeoWe
+elif user_month == "August" and user_day >= 23: 
+  astrology_sign = "Virgo"
+  strengths = VirgoStr
+  weaknesses = VirgoWe
+elif user_month == "September" and user_day <= 22:
+  astrology_sign = "Virgo"
+  strengths = VirgoStr
+  weaknesses = VirgoWe
+elif user_month == "September" and user_day >= 23:
+  astrology_sign = "Libra"
+  strengths = LibraStr
+  weaknesses = LibraWe
+elif user_month == "October" and user_day <= 22:
+  astrology_sign = "Libra"
+  strengths = LibraStr
+  weaknesses = LibraWe
+elif user_month == "October" and user_day >= 23:
+  astrology_sign = "Scorpio"
+  strengths = ScorpioStr
+  weaknesses = ScorpioWe
+elif user_month == "November" and user_day <= 21:
+  astrology_sign = "Scorpio"
+  strengths = ScorpioStr
+  weaknesses = ScorpioWe
+elif user_month == "November" and user_day >= 22:
+  astrology_sign = "Sagittarius"
+  strengths = SagittariusStr
+  weaknesses = SagittariusWe
+elif user_month == "December" and user_day <= 21:
+  astrology_sign = "Sagittarius"
+  strengths = SagittariusStr
+  weaknesses = SagittariusWe
+elif user_month == "December" and user_day >= 22:
+  astrology_sign = "Capricorn"
+  strengths = CapricornStr
+  weaknesses = CapricornWe
+elif user_month == "January" and user_day <= 19:
+  astrology_sign = "Capricorn"
+  strengths = CapricornStr
+  weaknesses = CapricornWe
+else: 
+  print("enter a valid birthday please :)")
+
+
+print("your astrology sign is " + astrology_sign + "!!! \nRead below for your strengths and weaknesses:")
+# weaknesses and strengths based on the sign 
+#Aries Taurus Gemini Cancer Leo Virgo Libra Scorpio Sagittarius Capricorn Aquarius Pisces
+print("Strengths: " + strengths)
+print("Weaknesses: " + weaknesses)
+
+    
 
 
 
@@ -86,15 +287,6 @@ import random
 
 
 
-
-
-
-
-
-
-
-
-
 # -------------------------------------------- 
 
 	# Part 3: Customize it!
@@ -102,20 +294,4 @@ import random
 	# Select your own theme and use case and modify your code!
 	
 # -------------------------------------------- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
--- functions.py
+++ functions.py
@@ -20,30 +20,63 @@ print("------------------- Challenge 1 -------------------")
 # **** Challenge 1: Problem 1 ****
 # Write a function called print_message() that prints any message you want.
 
+def print_message(statement):
+	print(statement)
 
+print_message("hello world!")
 # **** Challenge 1: Problem 2 ****
 # Write a function called print_five_messages() that calls print_message() five times.
 
+def print_five_messages(statement): 
+	print_message(statement)
+	print_message(statement)
+	print_message(statement)
+	print_message(statement)
+	print_message(statement)
+
+print_five_messages("hello world x5!")
 
 # **** Challenge 1: Problem 3 ****
 # Write a function called get_user_input() that asks the user if they'd like to print your message
 # once or five times. Then call one of the two functions above based on what the user decides.
 
+def get_user_input(statement): 
+	decision = input("Would you like to print your message once or five times?")
+		# print 5 times if the user enters 5
+	if(decision == str(5)):
+		print_five_messages(statement)
+		# otherwise only print it once
+	elif(decision == str(1)):
+		print_message(statement)
+	else: 
+		print("please enter 1 or 5 :(")
 
+print(get_user_input(input()))
 # **** Challenge 1: Problem 4 ****
 # Write a function called print_greeting() that prints a greeting message to the user.
 
+def print_greeting(): 
+	print("Hello! Welcome to Google Code Next!")
+
+print_greeting()
 
 # **** Challenge 1: Problem 5 ****
 # Write a function called print_closing() that prints a goodbye message to the user.
+
+def print_closing(): 
+	print("Goodbye! See you next time!")
 
 
 # **** Challenge 1: Problem 6 ****
 # Write a function called run() that greets the user, asks them for input, and sends a goodbye message.
 # Remember! Use the functions that you've already made. Don't hardcode anything!
 
+def run(statement): 
+	print_greeting()
+	get_user_input(statement)
+	print_closing() 
 
-
+run(input())
 # -------------------------------------------- 
 
 # Challenge 2: Functions are also able to take input and return output. 
@@ -65,9 +98,19 @@ print("------------------- Challenge 2 -------------------")
 
 # -------------------------------------------- 
 
+def sum_double(num1, num2): 
+	if num1 != num2: 
+		return num1 + num2
+	else:
+		# this ,means 
+		return 2 * (num1 + num2)
+		
 
+print(sum_double(1,2))
+print(sum_double(3,2))
+print(sum_double(2,2))
 
-
+# works good
 
 
 
@@ -86,9 +129,17 @@ print("------------------- Challenge 2 -------------------")
 
 # -------------------------------------------- 
 
+def makes_10(a, b): 
+	if a == 10 or b == 10: 
+		return True
+	elif a + b == 10:
+		return True
+	else: 
+		return False
 
-
-
+print(makes_10(9, 10))
+print(makes_10(9, 9))
+print(makes_10(1, 9))
 
 
 # Make sure to test your code! Write a few function calls to make sure your code works!
@@ -110,10 +161,20 @@ print("------------------- Challenge 2 -------------------")
 		# alarm_clock(0, False) → "10:00"
 
 # -------------------------------------------- 
+# num from 0-6
 
+def alarm_status(dayOfTheWeek, bool):
+	if bool == True:
+		return "off"
+	elif dayOfTheWeek >= 1 and dayOfTheWeek <= 5:
+		return "7:00"
+	else: 
+		# dayOfTheWeek has to be 6 or 0, Sat or Sun respectively
+		return "10:00"
 
-
-
+print(alarm_status(1, False))
+print(alarm_status(6, True))
+print(alarm_status(0, False))
 
 
 
@@ -136,7 +197,18 @@ print("------------------- Challenge 2 -------------------")
 
 # -------------------------------------------- 
 
+def received_speed_ticket(num_speed):
+	if num_speed <= 60 and num_speed >= 0: 
+		return 0 
+	elif num_speed >= 61 and num_speed <= 80:
+		return 1
+	else: 
+		return 2
+		# here, the speed would be 81 or more 
 
+print(received_speed_ticket(58))
+print(received_speed_ticket(70))
+print(received_speed_ticket(109))
 
 
 
